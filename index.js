baa9b4661053101dcbddc5deb9eefba0de14605e
Saad Nasir <saadxnasir@gmail.com>
2024-09-04 01:00:55 +0200

    Changed welcome message in index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World from Node.js!'));
+app.get('/', (req, res) => res.send('Hi I am Saad and this is my demo app!'));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
